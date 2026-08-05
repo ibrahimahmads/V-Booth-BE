21ffe5ebc59d71d930aef558cf294506b0fafe77
Ibrahim Ahmad <ibrahimahmad593@gmail.com>
2026-08-05 23:16:20 +0700

    feat: update cors config

--- src/main/java/com/ibrahimahmads/github/vbooth/config/CorsConfig.java
+++ src/main/java/com/ibrahimahmads/github/vbooth/config/CorsConfig.java
@@ -1,12 +1,18 @@
 package com.ibrahimahmads.github.vbooth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,7 +20,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOriginPatterns(allowedOrigins.toArray(new String[0]))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
